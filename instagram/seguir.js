709c89d618d45a64591e3c8d7ee0b20221782b26
Pedro Duarte <pedrohlduarte@gmail.com>
2023-02-05 19:39:05 -0300

    fix: seguir do instagram com correcoes

--- instagram/seguir.js
+++ instagram/seguir.js
@@ -1,5 +1,5 @@
-var value = 0
-var valueRound = 0
+let value = 0
+let valueRound = 0
 
 function delay(){
   value = (Math.random() * 4)
@@ -9,13 +9,45 @@ function delay(){
   });
 }
 
+function delayHour(){
+  return new Promise(function(resolve){
+    setTimeout(resolve,1000 * 60 * 60)
+  });
+}
+
+let xpath = "/html/body/div[2]/div/div/div/div[2]/div/div/div[2]/div/div/div[1]/div/div[2]/div/div/div/div/div[2]/div/div/div[3]/button[1]";
+
+function getElementByXpath(path) {
+  return document.evaluate(
+    path,
+    document,
+    null,
+    XPathResult.FIRST_ORDERED_NODE_TYPE,
+    null
+  ).singleNodeValue;
+}
+
 async function seguirStart(){
-  for (var i = 21; i < 100; i++) {
-    const button = document.querySelectorAll("button")[i]
-    button.click()
-    console.log(i)
-    console.log(button)
-    await delay();
+  for (let i = 1; i < 100; i++) {
+    console.log('i', i)
+    for (let j = ( i * 10 ) + 5; j <= ( i * 10 ) + 10; j++) {
+      console.log('j', j)
+      const button = document.querySelectorAll("button")[j]
+      button.click()
+      console.log(j)
+      console.log(button)
+      await delay();
+      let element = getElementByXpath(xpath);
+      if (!element) {
+        throw new Error("Error: cannot find an element with XPath(" + xpath + ")");
+      }
+      element.click();
+      await delay();
+    }
+    let today = new Date();
+    let time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    console.log(time)
+    await delayHour();
   }
 }
 
